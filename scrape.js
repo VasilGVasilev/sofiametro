eeeb2eab5a8fcf361067c4ff25647c06c93d359f
VasilGVasilev <vaskomeil@gmail.com>
2022-05-10 14:07:42 +0300

    plans for scraping

---
+++ scrape.js
@@ -0,0 +1,5 @@
+// it first opens the relevant station page
+
+// it either puts all numbers into an array and checks for closest number
+// or
+// it checks table based on current time -> hour; then look for minutes in subtable

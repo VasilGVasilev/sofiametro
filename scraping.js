275dc03a983907e746cba7eaa3e250d5480d24ee
VasilGVasilev <vaskomeil@gmail.com>
2022-05-15 12:45:00 +0300

    I forgot to initalise npm before using cheerios so this resolves the missing packages

--- scraping.js
+++ scraping.js
@@ -8,15 +8,16 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('https://www.youtube.com/watch?v=LoziivfAAjE', (error, response, html) => {
+request('https://schedules.sofiatraffic.bg/metro/M3#sign/4424/3320', (error, response, html) => {
     if (!error && response.statusCode == 200) {
         // const $ like jQuery
         const $ = cheerio.load(html);
-        const firstTime = $('#text')
+        const firstTime = $('/html/body/div[2]/div[2]/div/div/div/div[2]/div/div/div/div[1]/div[2]/div/div[2]/table/tbody/tr/td[2]/div/a[1]')
         // !!!
         // the problem is that the table doesnt update with the link and it scrapes the defualt first station table
         // also move to another lib other than request
         // !!!
+        
         let s = firstTime.text();
         console.log(s.trim());
 

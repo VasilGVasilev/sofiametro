0063be6d5b581a3cd209056a22dbb5ad2c308d4f
VasilGVasilev <vaskomeil@gmail.com>
2022-07-14 18:34:09 +0300

    all major issues in scraper fixed

--- scraping.js
+++ scraping.js
@@ -15,7 +15,7 @@ module.exports =  (async () => {
 
   // {waituntil + timeout} are crucial for navigating up to the precise moment of final state of rendering of a dynamic div
   // SUBJECT TO OPTIMISATION when in production -> when front is attached to back, try to catch error of exceeding max timeout by prompting a restart
-  await page.goto('https://schedules.sofiatraffic.bg/metro/M3#sign/4424/3320', {waitUntil: 'load', timeout: 3000});
+  await page.goto('https://schedules.sofiatraffic.bg/metro/M3#sign/4424/3320', {waitUntil: 'networkidle2'});
   
   //extract info from all table rows and map them onto a new array
   let hours = await page.evaluate(() => {
@@ -27,65 +27,63 @@ module.exports =  (async () => {
 
   // Tried arr.push and arrow function but .replace() TypeError due to hours[i] undefined in for loop
   //  => seamless .map() solution
-  
   const hourNums = hours.map(el => Number(el.replace(":", "")));
   
-
   
   // current time
-  var current = new Date();
-  var h = current.getHours();
-  var m = current.getMinutes();
+  let current = new Date();
+  let h = current.getHours();
+  let m = current.getMinutes();
+  m = addZero(m);
   let currentTime = Number('' + h + m);
+
+  // to not lose minutes 18:09 be 189
+  function addZero(time){
+    if (time < 10){
+      time = '0' + time;
+    }
+    return time;
+  }
   
   // next two trains
   let firstTrain = 0;
   let secondTrain = 0;
   for (let i = 0; i < hourNums.length; i++){
-    if (currentTime < hourNums[i]){
-      firstTrain = hourNums[i];
-      secondTrain = hourNums[i+1];
+    if (currentTime <= hourNums[i]){
+      // bug fix of 100 to 60 min format -> from 7:59 to 8:00 is 1 min not 41 mins
+      if ((hourNums[i] - currentTime) > 40){
+        firstTrain = hourNums[i] - 40;
+      } else {
+        firstTrain = hourNums[i];
+        secondTrain = hourNums[i+1];
+      }
+      if (hourNums[i+1] - currentTime > 40){
+        secondTrain = hourNums[i+1] - 40;
+      } 
       break;
     }
   }
-  console.log(currentTime);
-  console.log(firstTrain);
-  console.log(secondTrain);
 
   // format train arrival in minutes to pass
   let firstTrainComesIn = firstTrain - currentTime;
   let secondTrainComesIn = secondTrain - currentTime;
 
+
   // check for metro not working
   let startWorking = hourNums[0];
   let stopWorking = hourNums[hourNums.length - 1];
-  console.log(startWorking);
-  console.log(stopWorking); 
 
-  console.log(`First Train comes in: ${firstTrainComesIn} mins...`);
-  console.log(`Second Train comes in: ${secondTrainComesIn} mins...`);
 
-  // // next two trains
-  // let firstN = 0;
-  // let secondN = 0;
+  // FIX BUG - you dismiss the last train if it is after 0:00
+  if (currentTime > startWorking || currentTime < stopWorking) {
+    console.log(`First Train comes in: ${firstTrainComesIn} mins...`);
+    if (secondTrainComesIn > 0){
+      console.log(`Second Train comes in: ${secondTrainComesIn} mins...`);
+    }
 
-  // let i = 0;
-  // while (hourNums[i] < currentTime) {
-  //   i++
-  //   firstN = hourNums[i];
-  //   let l = i + 1;
-  //   secondN = hourNums[l];
-  // }
-  
-  // metro offtime disclaimer
-  // if (typeof firstN === 'undefined' && typeof secondN === 'undefined' || firstN === 0 && secondN === 0) {
-  //   return "Metro Not working"
-  // } else {
-  //   console.log(firstN);
-  //   console.log(secondN);
-  // }
-
-  
+  } else {
+    console.log(`Metro is not working.`);
+  }
   
   await browser.close();
 })();
